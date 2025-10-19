2a33a48517ae8840b6afca1d976c81375b3f4190
Milton Bueno <miltonjunior17@gmail.com>
2025-10-19 01:03:41 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,5 @@
 package boardgame;
 
-import chess.Color;
-
 public class Board {
 
 	private int rows;
@@ -37,6 +35,11 @@ public class Board {
 	
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 	
 }
